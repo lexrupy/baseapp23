188e631b2a43e5c117702e2abdd048d24356b7fc
Lexrupy <lexrupy@gmail.com>
2009-07-20 00:38:21 -0300

    Added some more tests

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,27 +2,146 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Admin::UsersController do
 
-  it "Should show the signup screen with login layout"
+  def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs)
+  end
 
-  it "Should signup a new user and send an activation code by email"
+  before :each do
+    do_authorize
+  end
 
-  it "Should activate the user that confirms the email address with correct code"
+  describe "responding to GET index" do
 
-  it "Should help user with troubles"
+    it "should expose all users as @users" do
+      User.stubs(:paginate).returns([mock_user])
+      get :index
+      assigns[:users].should == [mock_user]
+    end
+  end
 
-  it "Should show the idiot (dear customer) how to rescue a complete lost account"
+  describe "responding to GET show" do
 
-  it "Should reset and email user a new password if usre remember the login or email"
+    it "should expose the requested user as @user" do
+      User.stubs(:find).with("37").returns(mock_user)
+      get :show, :id => "37"
+      assigns[:user].should equal(mock_user)
+    end
+  end
 
-  it "Should provide to autheticate user a way to change his password"
+  describe "responding to GET new" do
 
-  it "Should update the user password with valid credentials"
+    it "should expose a new user as @user" do
+      User.stubs(:new).returns(mock_user)
+      get :new
+      assigns[:user].should equal(mock_user)
+    end
 
-  it "Should provide to autheticate user a way to change his email"
+  end
 
-  it "Should update the user email with valid credentials"
+  describe "responding to GET edit" do
 
-  it "Should permit user to delete his own account"
+    it "should expose the requested user as @user" do
+      User.stubs(:find).with("37").returns(mock_user)
+      get :edit, :id => "37"
+      assigns[:user].should equal(mock_user)
+    end
+
+  end
+
+  describe "responding to POST create" do
+
+    describe "with valid params" do
+
+      it "should expose a newly created user as @user" do
+        User.stubs(:new).with({'these' => 'params'}).returns(mock_user(:register! => true))
+        post :create, :user => {:these => 'params'}
+        assigns(:user).should equal(mock_user)
+      end
+
+      it "should redirect to the created user" do
+        User.stubs(:new).returns(mock_user(:register! => true))
+        post :create, :user => {}
+        response.should redirect_to(admin_user_url(mock_user))
+      end
+
+    end
+
+    describe "with invalid params" do
+
+      it "should expose a newly created but unsaved user as @user" do
+        User.stubs(:new).with({'these' => 'params'}).returns(mock_user(:register! => true))
+        post :create, :user => {:these => 'params'}
+        assigns(:user).should equal(mock_user)
+      end
+
+      it "should re-render the 'new' template" do
+        User.stubs(:new).returns(mock_user(:register! => false))
+        post :create, :user => {}
+        response.should render_template('new')
+      end
+
+    end
+
+  end
+
+  describe "responding to PUT udpate" do
+
+    def stubs_user
+      mcuser = mock_user(:save => true)
+      mcuser.expects(:"role_ids=").with([1])
+      mcuser.expects(:"resource_ids=").with([1])
+      User.stubs(:find).returns(mcuser)
+      mcuser
+    end
+
+    describe "with valid params" do
+
+      it "should update the requested user" do
+        stubs_user
+        put :update, :user => {:role_ids => [1], :resource_ids => [1]}
+      end
+
+      it "should expose the requested user as @user" do
+        stubs_user
+        put :update, :user => {:role_ids => [1], :resource_ids => [1]}
+        assigns(:user).should equal(mock_user)
+      end
+
+      it "should redirect to the user" do
+        stubs_user
+        put :update, :user => {:role_ids => [1], :resource_ids => [1]}
+        response.should redirect_to(admin_user_url(mock_user))
+      end
+
+    end
+
+    describe "with invalid params" do
+
+      it "should re-render the 'edit' template" do
+        stubs_user.stubs(:save).returns(false)
+        put :update, :user => {:role_ids => [1], :resource_ids => [1]}
+        response.should render_template('edit')
+      end
+
+    end
+
+  end
+
+  describe "responding to DELETE destroy" do
+
+    it "should destroy the requested user" do
+      User.stubs(:find).with("37").returns(mock_user)
+      mock_user.stubs(:delete!)
+      delete :destroy, :id => "37"
+    end
+
+    it "should redirect to the users list" do
+      User.stubs(:find).returns(mock_user(:delete! => true))
+      delete :destroy, :id => "1"
+      response.should redirect_to(admin_users_url)
+    end
+
+  end
 
 end
 
